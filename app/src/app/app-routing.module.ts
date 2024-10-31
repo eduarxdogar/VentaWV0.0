fd80c0ebf31ded83f34649e11ccc8597d3fa5d75
eduarxdogar <cristiangarzon1224@hotmail.com>
2024-10-31 17:03:32 -0600

    VentaWv0.0

---
+++ app/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ClienteComponent } from './cliente/cliente.component';
+
+const routes: Routes = [
+  { path : '', redirectTo:'/home', pathMatch:'full'},
+
+  { path : 'cliente', component : ClienteComponent},
+  
+  { path : 'home', component : HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
